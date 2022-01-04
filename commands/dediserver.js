4f3307e9c4939afc3d927ad88801c687f5866678
Slycir <otto.hillegass@westtown.edu>
2022-01-03 20:38:27 -0500

    Video Link

--- commands/dediserver.js
+++ commands/dediserver.js
@@ -9,9 +9,13 @@ module.exports = {
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
-                    .setLabel('Dedicated Server Help')
+                    .setLabel('Dedicated Server Overview')
                     .setStyle('LINK')
                     .setURL('https://github.com/R2Northstar/Northstar/wiki/Hosting-a-Server#:~:text=Hosting%20a%20Dedicated%20Server'),
+                new MessageButton()
+                    .setLabel('Dedicated Server Video')
+                    .setStyle('LINK')
+                    .setURL('https://youtu.be/EZ3w2Nl9SZo')
             );
         return interaction.reply({ content: 'Dedicated server setup guide:', components: [row]});
     },
