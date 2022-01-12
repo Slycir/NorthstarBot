3d6e68cb8ca3d45a05f507a9e988d8835ab5cd30
Slycir <otto.hillegass@westtown.edu>
2022-01-12 01:11:46 -0500

    Expandable to guilds

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,8 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+for(x = 0; x < guildId.length; x++){
+	rest.put(Routes.applicationGuildCommands(clientId, guildId[x]), { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);
+}
